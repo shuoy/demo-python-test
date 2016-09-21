db463e5e546f26f04757bee0689d1fff17d4c3f7
Yang <syang1@ea.com>
2016-09-21 00:01:21 -0700

    add tests

---
+++ setup.py
@@ -0,0 +1,4 @@
+from distutils.core import setup, Extension
+setup(name='python-demo',
+      version='1.0',
+      )
---
+++ test_app.py
@@ -0,0 +1,9 @@
+from math import sqrt
+
+def test_numbers_4():
+    print "test start here"
+    assert sqrt(4) == 2
+
+def test_numbers_9():
+    print "test start here"
+    assert sqrt(9) == 3
